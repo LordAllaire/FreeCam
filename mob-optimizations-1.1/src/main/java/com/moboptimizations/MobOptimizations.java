227b13242a50b120c4a417ea6cae4e294f580502
LordAllaire <robertddallaire@gmail.com>
2026-05-10 15:29:55 -0400

    Add files via upload

---
+++ mob-optimizations-1.1/src/main/java/com/moboptimizations/MobOptimizations.java
@@ -0,0 +1,10 @@
+package com.moboptimizations;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MobOptimizations implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Mob Optimizations loaded.");
+    }
+}
